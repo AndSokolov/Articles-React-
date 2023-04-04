e633c6f404e89e0e90533d53b2e16f4ce209da3d
Andrey Sokolov <expertwed@gmail.com>
2023-04-04 10:34:40 +0300

    [feat] - test

--- src/admin-layout/constants/constants.ts
+++ src/admin-layout/constants/constants.ts
@@ -1,2 +1,4 @@
 export const API_KEY = "AIzaSyDRuoRH3cBbvODH0eLOrOKjbTdBgJuQ794";
 export const AUTH_API_URL = 'https://identitytoolkit.googleapis.com/v1';
+export const AUTH_API_URdL = 'https://identitytoolkit.googleapis.com/v1';
+
